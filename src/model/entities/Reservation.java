f5da5e5d228d8e688254359f3d744cd4aa2b99e9
Vinicius Avelino <vinicius.silva120@gmail.com>
2021-09-22 19:17:43 -0300

    Very bad solution to this problem - All resolution in program.java

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,55 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime(); // subtrai as datas pelos milisegundos
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // Converte o que está em milissegundos para dias
+
+	}
+
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nights";
+	}
+
+}
